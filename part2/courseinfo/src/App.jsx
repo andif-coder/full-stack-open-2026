5d4a0fc295f78143ac1dc30a94903d209d56eefa
chenweijie <fzucwj@gmail.com>
2026-07-30 14:45:26 +0800

    E2.5 of part2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,4 @@
-const Header = (props) => {
-	return (
-		<>
-			<h2>{props.course.name}</h2>
-		</>
-	)
-}
-const Part = (props) => {
-	return (
-		<p>
-			{props.name} {props.exercises}
-		</p>
-	)
-}
-const Content = (props) => {
-	return  (
-		<>
-		{
-			props.course.parts.map( part => <Part key={part.id} name={part.name} exercises = {part.exercises} /> )
-		}
-		</>
-	)
-}
-const Total = (props) =>{
-	const total = props.course.parts.reduce((cur, part) => {
-		return cur + part.exercises
-	}, 0)
-	return (
-		<p><b> total of {total} exercises </b></p>
-	)
-}
-const Course = (props) => {
-  return (
-    <div>
-			<Header course={props.course} />
-			<Content course={props.course} />
-			<Total course={props.course} />
-    </div>
-  )	
-}
-
+import Course from './course'
 const App = () => {
   const courses = [
     {
