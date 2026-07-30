eba548d7e652e0d8a2c473df383bcf63476f2fe5
chenweijie <fzucwj@gmail.com>
2026-07-30 19:55:45 +0800

    E2.7 of part2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -17,6 +17,13 @@ const App = () => {
 	}
 	const addPerson = (event) => {
 		event.preventDefault()
+		if (persons.length > 0) {
+			const exist = persons.some(person => person.name == newName)
+			if (exist) {
+				alert(newName + " is already added to phonebook")
+				return
+			}
+		}
 		const newPerson = {
 			name: newName
 		}
