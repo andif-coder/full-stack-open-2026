b846f34c11f0dcebddc3c18ae0d526d24b4ece13
chenweijie <fzucwj@gmail.com>
2026-08-04 17:09:36 +0800

    E3.5 of part3

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 let persons = [
 	{ 
 	  "id": "1",
@@ -46,6 +47,26 @@ app.delete('/api/persons/:id', (request, response) => {
 	const id = String(request.params.id)
 	persons = persons.filter(p => p.id !== id)
 	response.status(204).end()
+})
+const gerenateId = () => {
+	const newId = Math.floor(Math.random() * 1000000)
+	return String(newId)
+}
+app.post('/api/persons', (request, response) => {
+	const body = request.body
+	if (body.name && body.number) {
+		const newPerson = {
+			name: body.name,
+			number: body.number,
+			id: gerenateId()
+		}
+		persons = persons.concat(newPerson)
+		response.json(newPerson)
+	} else {
+		return response.status(400).json({
+			error: "name or number missing"
+		})
+	}
 })
 const PORT = 3001
 app.listen(PORT, () => {
