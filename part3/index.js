b19c74bb6d2c98b9266cdea28b1d57e849afd3f7
chenweijie <fzucwj@gmail.com>
2026-08-05 09:41:12 +0800

    deploy model in backend

--- part3/index.js
+++ part3/index.js
@@ -7,6 +7,7 @@ morgan.token('body', (req) => {
 })
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :response-time ms :body'))
 let persons = [
 	{ 
