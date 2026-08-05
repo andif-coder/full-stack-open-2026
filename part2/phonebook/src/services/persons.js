80926440a7af6a592e728008ee5caffe66723faa
chenweijie <fzucwj@gmail.com>
2026-08-05 10:13:26 +0800

    configure dev server proxy for /api endpoints

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://backend-lw5i.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 const getAll = () => {
 	return axios.get(baseUrl).then(response => response.data)
 }
