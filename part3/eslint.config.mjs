bf88942842bb5ddbab5ba020c59c55dda88a2d76
chenweijie <fzucwj@gmail.com>
2026-08-06 17:16:35 +0800

    update config of eslint

--- part3/eslint.config.mjs
+++ part3/eslint.config.mjs
@@ -12,23 +12,21 @@ export default [
       ecmaVersion: 'latest',
     },
     plugins: {
-      '@stylistic/js': stylisticJs, // 注册插件命名空间
+      '@stylistic/js': stylisticJs,
     },
     rules: {
-      // 1. 强制使用 2 个空格缩进
       '@stylistic/js/indent': ['error', 2],
-      
-      // 2. 强制换行符使用 Unix 格式 (\n / LF)
       '@stylistic/js/linebreak-style': ['error', 'unix'],
-      
-      // 3. 强制在代码中使用单引号
       '@stylistic/js/quotes': ['error', 'single'],
-      
-      // 4. 强制不在语句末尾加分号
       '@stylistic/js/semi': ['error', 'never'],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
   },
-	{
-		ignores: ['dist/**'],
-	}
+  {
+    ignores: ['dist/**'],
+  },
 ]
