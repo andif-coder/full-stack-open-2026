f2d2631a3968033d474348999567adbf9f769780
chenweijie <fzucwj@gmail.com>
2026-08-18 10:25:37 +0800

    add utils/for_testing.js

---
+++ part4/utils/for_testing.js
@@ -0,0 +1,8 @@
+const reverse = (string) => {
+	return string.split('').reverse().join('')
+}
+const average = (array) => {
+	if (array.length === 0) return 0
+	return array.reduce((sum, a) => { return sum + a }, 0) / array.length
+}
+module.exports = { reverse, average }
